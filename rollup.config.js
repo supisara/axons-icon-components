46b7b7ce01008bd759557f501ed19baec8a3e107
Jaelfff <supisara_12@hotmail.com>
2023-04-19 16:38:01 +0700

    secound commit

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import packageJson from "./package.json" assert { type: "json" };
-
-// const packageJson = require("./package.json");
+import postcss from "rollup-plugin-postcss";
 
 export default [
   {
@@ -25,11 +24,13 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(),
     ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
+    external: [/\.css$/],
   },
 ];
--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Icon.css";
 
 export interface ButtonProps {
   label: string;
